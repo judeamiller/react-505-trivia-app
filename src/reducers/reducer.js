bdeb3324521c6f495d4975c91a3e04beda8b61a3
Jude Baca-Miller <judeamiller@gmail.com>
2018-11-19 20:05:51 -0700

    add question to initial state.  Add scoring function.

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,19 +1,27 @@
 import * as  actions from   '../actions/actions';
 
 const initialState = {
-    score: 0
-
+    score: 0,
+    questions: [
+        {
+            question: 'React is a front end JS framework?',
+            answer: 'false',
+        },
+    ],  
 };
 
 export default function reducer(state = initialState, action) {
     switch (action.type) {
         case actions.SUBMIT_ANSWER:
+        let score = state.score;
+        const answer = action.answer === state.questions[0].answer ? score + 1 : score;
                 return{
                     ...state,
+                    score: answer,
                 };
 
                 default:
-                    return state;
-        }
+                    return state;     
+        };
     
 }
